1ed87f0ac691702f4f2c0829e87941d025b53b37
Ganesh Patil <ganeshkumar.patil@gmail.com>
2025-03-31 22:00:11 +0100

    intial version

---
+++ search_engine.py
@@ -0,0 +1,75 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_community.utilities import ArxivAPIWrapper, WikipediaAPIWrapper
+from langchain_community.tools import (
+    ArxivQueryRun,
+    WikipediaQueryRun,
+    DuckDuckGoSearchRun,
+)
+from langchain.agents import initialize_agent, AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+
+
+## Arxiv and wikipedia Tools
+arxiv_wrapper = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
+
+api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+wiki = WikipediaQueryRun(api_wrapper=api_wrapper)
+
+search = DuckDuckGoSearchRun(name="Search")
+
+
+st.title("🔎 LangChain - Chat with search")
+"""
+In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
+Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
+"""
+
+## Get the Groq API Key and url(YT or website)to be summarized
+with st.sidebar:
+    st.markdown("**Enter your Groq API Key**")
+    st.markdown(
+        "*Get your API key from the [Groq Console](https://console.groq.com/keys)*"
+    )
+    groq_api_key = st.text_input(
+        "Groq API Key", value="", type="password", label_visibility="collapsed"
+    )
+
+# groq_api_key = st.secrets["api_keys"]["GROQ_API_KEY"]
+
+if groq_api_key:
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [
+            {
+                "role": "assisstant",
+                "content": "Hi,I'm a chatbot who can search the web. How can I help you?",
+            }
+        ]
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if prompt := st.chat_input(placeholder="What is machine learning?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+
+        llm = ChatGroq(
+            groq_api_key=groq_api_key, model_name="Llama3-8b-8192", streaming=True
+        )
+        tools = [search, arxiv, wiki]
+
+        search_agent = initialize_agent(
+            tools,
+            llm,
+            agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            handling_parsing_errors=True,
+        )
+
+        with st.chat_message("assistant"):
+            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            st.write(response)
+else:
+    st.write("Please enter Groq key to perform a search")
